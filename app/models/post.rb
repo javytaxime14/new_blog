abe8799c9bcf7141d220a5f20ea4af35f23ee032
Javiera Hidalgo <javiera_56@hotmail.com>
2021-07-25 21:35:27 -0400

    adding p'osts and styling changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
     validates :title, :content, :image_url, presence: true
+    before_save :remove_word
+
+    def remove_word
+        self.content = self.content.remove(' spoiler')
+    end
 end
